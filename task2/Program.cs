0106aeb48470f25a9f54bfa57d57825cc7fd523d
fenixzava <fenixzava@gmail.com>
2022-10-11 01:20:51 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе удаляет вторую цифру этого числа.
+
+
+int Module(int module)
+
+{
+if (module < 0)
+{
+    module = -module;
+}
+return module;
+}   
+
+System.Console.WriteLine("Введите трехзначное чило: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+value = Module(value);
+if (value > 99 & value <=999)
+{
+   int CorrectValue = (value/100)*10+(value%10);
+   System.Console.WriteLine($"Выводим трехзначное число без десятков: {CorrectValue}");
+}
+else 
+{
+    System.Console.WriteLine("Это не трехзначное число");
+}
